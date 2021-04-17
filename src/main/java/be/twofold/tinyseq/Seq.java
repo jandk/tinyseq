7112cf82fadf18918a8e43989263c1fc90b70f42
Jan De Kock <jan@twofold.be>
2021-04-17 21:21:07 +0200

    Add namespace

---
+++ src/main/java/be/twofold/tinyseq/Seq.java
@@ -0,0 +1,5 @@
+package be.twofold.tinyseq;
+
+@FunctionalInterface
+public interface Seq<T> extends Iterable<T> {
+}
---
+++ src/test/java/be/twofold/tinyseq/SeqTest.java
@@ -0,0 +1,14 @@
+package be.twofold.tinyseq;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SeqTest {
+
+    @Test
+    void testSeq() {
+        assertThat(true).isTrue();
+    }
+
+}
