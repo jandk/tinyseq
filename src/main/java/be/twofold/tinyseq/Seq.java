f681bf6d54b15dfc1eaf67f46f5e9992558373cb
Jan De Kock <jan.dekock@vdab.be>
2021-08-30 22:09:58 +0200

    Reorder methods

--- src/main/java/be/twofold/tinyseq/Seq.java
+++ src/main/java/be/twofold/tinyseq/Seq.java
@@ -9,18 +9,8 @@ import static be.twofold.tinyseq.SeqHelper.*;
 @FunctionalInterface
 public interface Seq<T> extends Iterable<T> {
 
-    // region Factory Methods
-
     static <T> Seq<T> empty() {
         return Collections::emptyIterator;
-    }
-
-    static <T> Seq<T> seq(Iterable<T> iterable) {
-        return iterable::iterator;
-    }
-
-    static <T> Seq<T> seq(Iterator<T> iterator) {
-        return ((Seq<T>) () -> iterator).once();
     }
 
     @SafeVarargs
@@ -28,121 +18,12 @@ public interface Seq<T> extends Iterable<T> {
         return seq(Arrays.asList(elements));
     }
 
-    // endregion
-
-    // region Intermediate Operations
-
-    default Seq<T> distinct() {
-        return () -> new DistinctItr<>(iterator());
+    static <T> Seq<T> seq(Iterator<T> iterator) {
+        return ((Seq<T>) () -> iterator).once();
     }
 
-    default Seq<T> drop(int count) {
-        return () -> new DropItr<>(iterator(), count);
-    }
-
-    default Seq<T> filter(Predicate<? super T> predicate) {
-        Objects.requireNonNull(predicate, "predicate is null");
-
-        return () -> new FilterItr<>(iterator(), predicate);
-    }
-
-    default Seq<T> filterIndexed(BiPredicate<Integer, ? super T> predicate) {
-        Objects.requireNonNull(predicate, "predicate is null");
-
-        AtomicInteger index = new AtomicInteger();
-        return filter(t -> predicate.test(index.getAndIncrement(), t));
-    }
-
-    default <R> Seq<R> flatMap(Function<? super T, ? extends Iterable<? extends R>> mapper) {
-        Objects.requireNonNull(mapper, "mapper is null");
-
-        return () -> new FlatMapItr<>(iterator(), mapper);
-    }
-
-    default <R> Seq<R> flatMapIndexed(BiFunction<Integer, ? super T, ? extends Iterable<? extends R>> mapper) {
-        Objects.requireNonNull(mapper, "mapper is null");
-
-        AtomicInteger index = new AtomicInteger();
-        return flatMap(t -> mapper.apply(index.getAndIncrement(), t));
-    }
-
-    default <R> Seq<R> map(Function<? super T, ? extends R> mapper) {
-        Objects.requireNonNull(mapper, "mapper is null");
-
-        return () -> new MapItr<>(iterator(), mapper);
-    }
-
-    default <R> Seq<R> mapIndexed(BiFunction<Integer, ? super T, ? extends R> mapper) {
-        Objects.requireNonNull(mapper, "mapper is null");
-
-        AtomicInteger index = new AtomicInteger();
-        return map(t -> mapper.apply(index.getAndIncrement(), t));
-    }
-
-    default Seq<T> onEach(Consumer<? super T> action) {
-        Objects.requireNonNull(action, "action is null");
-
-        return map(element -> {
-            action.accept(element);
-            return element;
-        });
-    }
-
-    default Seq<T> onEachIndexed(BiConsumer<Integer, ? super T> action) {
-        Objects.requireNonNull(action, "action is null");
-
-        AtomicInteger index = new AtomicInteger();
-        return map(element -> {
-            action.accept(index.getAndIncrement(), element);
-            return element;
-        });
-    }
-
-    default Seq<T> once() {
-        if (this instanceof OnceSeq) {
-            return this;
-        }
-        return new OnceSeq<>(this);
-    }
-
-    @SuppressWarnings("unchecked")
-    default Seq<T> sorted() {
-        return sorted((Comparator<? super T>) Comparator.naturalOrder());
-    }
-
-    default Seq<T> sorted(Comparator<? super T> comparator) {
-        Objects.requireNonNull(comparator, "comparator is null");
-
-        return () -> {
-            List<T> list = toList();
-            list.sort(comparator);
-            return list.iterator();
-        };
-    }
-
-    default Seq<T> take(int count) {
-        return () -> new TakeItr<>(iterator(), count);
-    }
-
-    // endregion
-
-
-    default boolean any(Predicate<? super T> predicate) {
-        for (T element : this) {
-            if (predicate.test(element)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    default boolean none(Predicate<? super T> predicate) {
-        for (T element : this) {
-            if (predicate.test(element)) {
-                return false;
-            }
-        }
-        return true;
+    static <T> Seq<T> seq(Iterable<T> iterable) {
+        return iterable::iterator;
     }
 
     default boolean all(Predicate<? super T> predicate) {
@@ -154,110 +35,13 @@ public interface Seq<T> extends Iterable<T> {
         return true;
     }
 
-    default int count() {
-        int count = 0;
-        for (T ignored : this) {
-            count++;
-        }
-        return count;
-    }
-
-    default int count(Predicate<? super T> predicate) {
-        Objects.requireNonNull(predicate, "predicate is null");
-
-        int count = 0;
+    default boolean any(Predicate<? super T> predicate) {
         for (T element : this) {
             if (predicate.test(element)) {
-                count++;
+                return true;
             }
         }
-        return count;
-    }
-
-    // region aggregates
-
-    default int min(ToIntFunction<? super T> mapper) {
-        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Integer::min);
-    }
-
-    default long min(ToLongFunction<? super T> mapper) {
-        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Long::min);
-    }
-
-    default double min(ToDoubleFunction<? super T> mapper) {
-        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Double::min);
-    }
-
-    default OptionalInt minOptional(ToIntFunction<? super T> mapper) {
-        return optionalIterator(this)
-            .map(it -> OptionalInt.of(SeqHelper.reduce(it, mapper, Integer::min)))
-            .orElseGet(OptionalInt::empty);
-    }
-
-    default OptionalLong minOptional(ToLongFunction<? super T> mapper) {
-        return optionalIterator(this)
-            .map(it -> OptionalLong.of(SeqHelper.reduce(it, mapper, Long::min)))
-            .orElseGet(OptionalLong::empty);
-    }
-
-    default OptionalDouble minOptional(ToDoubleFunction<? super T> mapper) {
-        return optionalIterator(this)
-            .map(it -> OptionalDouble.of(SeqHelper.reduce(it, mapper, Double::min)))
-            .orElseGet(OptionalDouble::empty);
-    }
-
-    default int max(ToIntFunction<? super T> mapper) {
-        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Integer::max);
-    }
-
-    default long max(ToLongFunction<? super T> mapper) {
-        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Long::max);
-    }
-
-    default double max(ToDoubleFunction<? super T> mapper) {
-        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Double::max);
-    }
-
-    default OptionalInt maxOptional(ToIntFunction<? super T> mapper) {
-        return optionalIterator(this)
-            .map(it -> OptionalInt.of(SeqHelper.reduce(it, mapper, Integer::max)))
-            .orElseGet(OptionalInt::empty);
-    }
-
-    default OptionalLong maxOptional(ToLongFunction<? super T> mapper) {
-        return optionalIterator(this)
-            .map(it -> OptionalLong.of(SeqHelper.reduce(it, mapper, Long::max)))
-            .orElseGet(OptionalLong::empty);
-    }
-
-    default OptionalDouble maxOptional(ToDoubleFunction<? super T> mapper) {
-        return optionalIterator(this)
-            .map(it -> OptionalDouble.of(SeqHelper.reduce(it, mapper, Double::max)))
-            .orElseGet(OptionalDouble::empty);
-    }
-
-    default int sum(ToIntFunction<? super T> mapper) {
-        int sum = 0;
-        for (T element : this) {
-            sum += mapper.applyAsInt(element);
-        }
-        return sum;
-    }
-
-    default long sum(ToLongFunction<? super T> mapper) {
-        long sum = 0;
-        for (T element : this) {
-            sum += mapper.applyAsLong(element);
-        }
-        return sum;
-    }
-
-    default double sum(ToDoubleFunction<? super T> mapper) {
-        double sum = 0;
-        for (T element : this) {
-            sum += mapper.applyAsDouble(element);
-        }
-        return sum;
+        return false;
     }
 
     default double average(ToIntFunction<? super T> mapper) {
@@ -290,6 +74,223 @@ public interface Seq<T> extends Iterable<T> {
         return count == 0 ? Double.NaN : sum / count;
     }
 
+    default int count() {
+        int count = 0;
+        for (T ignored : this) {
+            count++;
+        }
+        return count;
+    }
+
+    default int count(Predicate<? super T> predicate) {
+        Objects.requireNonNull(predicate, "predicate is null");
+
+        int count = 0;
+        for (T element : this) {
+            if (predicate.test(element)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    default Seq<T> distinct() {
+        return () -> new DistinctItr<>(iterator());
+    }
+
+    default Seq<T> drop(int count) {
+        return () -> new DropItr<>(iterator(), count);
+    }
+
+    default Seq<T> filter(Predicate<? super T> predicate) {
+        Objects.requireNonNull(predicate, "predicate is null");
+
+        return () -> new FilterItr<>(iterator(), predicate);
+    }
+
+    default Seq<T> filterIndexed(BiPredicate<Integer, ? super T> predicate) {
+        Objects.requireNonNull(predicate, "predicate is null");
+
+        AtomicInteger index = new AtomicInteger();
+        return filter(t -> predicate.test(index.getAndIncrement(), t));
+    }
+
+    default T first() {
+        return nonEmptyIterator(this)
+            .next();
+    }
+
+    default Optional<T> firstOptional() {
+        return optionalIterator(this)
+            .map(Iterator::next);
+    }
+
+    default <R> Seq<R> flatMap(Function<? super T, ? extends Iterable<? extends R>> mapper) {
+        Objects.requireNonNull(mapper, "mapper is null");
+
+        return () -> new FlatMapItr<>(iterator(), mapper);
+    }
+
+    default <R> Seq<R> flatMapIndexed(BiFunction<Integer, ? super T, ? extends Iterable<? extends R>> mapper) {
+        Objects.requireNonNull(mapper, "mapper is null");
+
+        AtomicInteger index = new AtomicInteger();
+        return flatMap(t -> mapper.apply(index.getAndIncrement(), t));
+    }
+
+    default <U> U fold(U initial, BiFunction<? super U, ? super T, ? extends U> function) {
+        U result = initial;
+        for (T element : this) {
+            result = function.apply(result, element);
+        }
+        return result;
+    }
+
+    default T last() {
+        return SeqHelper.last(
+            nonEmptyIterator(this)
+        );
+    }
+
+    default Optional<T> lastOptional() {
+        return optionalIterator(this)
+            .map(SeqHelper::last);
+    }
+
+    default <R> Seq<R> map(Function<? super T, ? extends R> mapper) {
+        Objects.requireNonNull(mapper, "mapper is null");
+
+        return () -> new MapItr<>(iterator(), mapper);
+    }
+
+    default <R> Seq<R> mapIndexed(BiFunction<Integer, ? super T, ? extends R> mapper) {
+        Objects.requireNonNull(mapper, "mapper is null");
+
+        AtomicInteger index = new AtomicInteger();
+        return map(t -> mapper.apply(index.getAndIncrement(), t));
+    }
+
+    default int max(ToIntFunction<? super T> mapper) {
+        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Integer::max);
+    }
+
+    default long max(ToLongFunction<? super T> mapper) {
+        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Long::max);
+    }
+
+    default double max(ToDoubleFunction<? super T> mapper) {
+        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Double::max);
+    }
+
+    default OptionalInt maxOptional(ToIntFunction<? super T> mapper) {
+        return optionalIterator(this)
+            .map(it -> OptionalInt.of(SeqHelper.reduce(it, mapper, Integer::max)))
+            .orElseGet(OptionalInt::empty);
+    }
+
+    default OptionalLong maxOptional(ToLongFunction<? super T> mapper) {
+        return optionalIterator(this)
+            .map(it -> OptionalLong.of(SeqHelper.reduce(it, mapper, Long::max)))
+            .orElseGet(OptionalLong::empty);
+    }
+
+    default OptionalDouble maxOptional(ToDoubleFunction<? super T> mapper) {
+        return optionalIterator(this)
+            .map(it -> OptionalDouble.of(SeqHelper.reduce(it, mapper, Double::max)))
+            .orElseGet(OptionalDouble::empty);
+    }
+
+    default int min(ToIntFunction<? super T> mapper) {
+        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Integer::min);
+    }
+
+    default long min(ToLongFunction<? super T> mapper) {
+        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Long::min);
+    }
+
+    default double min(ToDoubleFunction<? super T> mapper) {
+        return SeqHelper.reduce(nonEmptyIterator(this), mapper, Double::min);
+    }
+
+    default OptionalInt minOptional(ToIntFunction<? super T> mapper) {
+        return optionalIterator(this)
+            .map(it -> OptionalInt.of(SeqHelper.reduce(it, mapper, Integer::min)))
+            .orElseGet(OptionalInt::empty);
+    }
+
+    default OptionalLong minOptional(ToLongFunction<? super T> mapper) {
+        return optionalIterator(this)
+            .map(it -> OptionalLong.of(SeqHelper.reduce(it, mapper, Long::min)))
+            .orElseGet(OptionalLong::empty);
+    }
+
+    default OptionalDouble minOptional(ToDoubleFunction<? super T> mapper) {
+        return optionalIterator(this)
+            .map(it -> OptionalDouble.of(SeqHelper.reduce(it, mapper, Double::min)))
+            .orElseGet(OptionalDouble::empty);
+    }
+
+    default boolean none(Predicate<? super T> predicate) {
+        for (T element : this) {
+            if (predicate.test(element)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    default Seq<T> onEach(Consumer<? super T> action) {
+        Objects.requireNonNull(action, "action is null");
+
+        return map(element -> {
+            action.accept(element);
+            return element;
+        });
+    }
+
+    default Seq<T> onEachIndexed(BiConsumer<Integer, ? super T> action) {
+        Objects.requireNonNull(action, "action is null");
+
+        AtomicInteger index = new AtomicInteger();
+        return map(element -> {
+            action.accept(index.getAndIncrement(), element);
+            return element;
+        });
+    }
+
+    default Seq<T> once() {
+        if (this instanceof OnceSeq) {
+            return this;
+        }
+        return new OnceSeq<>(this);
+    }
+
+    default T reduce(BinaryOperator<T> operator) {
+        return SeqHelper.reduce(
+            nonEmptyIterator(this), operator
+        );
+    }
+
+    default Optional<T> reduceOptional(BinaryOperator<T> operator) {
+        return optionalIterator(this)
+            .map(it -> SeqHelper.reduce(it, operator));
+    }
+
+    @SuppressWarnings("unchecked")
+    default Seq<T> sorted() {
+        return sorted((Comparator<? super T>) Comparator.naturalOrder());
+    }
+
+    default Seq<T> sorted(Comparator<? super T> comparator) {
+        Objects.requireNonNull(comparator, "comparator is null");
+
+        return () -> {
+            List<T> list = toList();
+            list.sort(comparator);
+            return list.iterator();
+        };
+    }
+
     default IntSummaryStatistics statistics(ToIntFunction<? super T> mapper) {
         IntSummaryStatistics statistics = new IntSummaryStatistics();
         for (T element : this) {
@@ -314,57 +315,33 @@ public interface Seq<T> extends Iterable<T> {
         return statistics;
     }
 
-    // endregion
-
-    // region fold/reduce
-
-    default <U> U fold(U initial, BiFunction<? super U, ? super T, ? extends U> function) {
-        U result = initial;
+    default int sum(ToIntFunction<? super T> mapper) {
+        int sum = 0;
         for (T element : this) {
-            result = function.apply(result, element);
+            sum += mapper.applyAsInt(element);
         }
-        return result;
+        return sum;
     }
 
-    default T reduce(BinaryOperator<T> operator) {
-        return SeqHelper.reduce(
-            nonEmptyIterator(this), operator
-        );
+    default long sum(ToLongFunction<? super T> mapper) {
+        long sum = 0;
+        for (T element : this) {
+            sum += mapper.applyAsLong(element);
+        }
+        return sum;
     }
 
-    default Optional<T> reduceOptional(BinaryOperator<T> operator) {
-        return optionalIterator(this)
-            .map(it -> SeqHelper.reduce(it, operator));
+    default double sum(ToDoubleFunction<? super T> mapper) {
+        double sum = 0;
+        for (T element : this) {
+            sum += mapper.applyAsDouble(element);
+        }
+        return sum;
     }
 
-    // endregion
-
-    // region first/last
-
-    default T first() {
-        return nonEmptyIterator(this)
-            .next();
+    default Seq<T> take(int count) {
+        return () -> new TakeItr<>(iterator(), count);
     }
-
-    default Optional<T> firstOptional() {
-        return optionalIterator(this)
-            .map(Iterator::next);
-    }
-
-    default T last() {
-        return SeqHelper.last(
-            nonEmptyIterator(this)
-        );
-    }
-
-    default Optional<T> lastOptional() {
-        return optionalIterator(this)
-            .map(SeqHelper::last);
-    }
-
-    // endregion
-
-    // region toCollection
 
     default <C extends Collection<? super T>> C toCollection(C destination) {
         Objects.requireNonNull(destination, "destination is null");
@@ -409,6 +386,5 @@ public interface Seq<T> extends Iterable<T> {
         }
     }
 
-    // endregion
 
 }
